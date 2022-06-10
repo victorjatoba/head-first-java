d5c996a79a15e3e58585547c64262bc72b0dc8b2
Victor Jatobá <jatoba@credifit.com.br>
2022-06-10 21:32:45 +0000

    add party example

--- src/Party.java
+++ src/Party.java
@@ -1,7 +1,7 @@
 import java.awt.*;
-import java.awt.event.*;
 
 class Party {
+
   public void buildInvite() {
     Frame f = new Frame();
     Label l = new Label("Party at Tim's");
@@ -9,5 +9,9 @@ class Party {
     Button c = new Button("Shoot me");
     Panel p = new Panel();
     p.add(l);
+    p.add(b);
+    p.add(c);
+    f.add(p);
+    f.setVisible(true);
   }
 }
---
+++ src/PartyTest.java
@@ -0,0 +1,7 @@
+class PartyTest {
+
+  public static void main(String args[]) {
+    Party p = new Party();
+    p.buildInvite();
+  }
+}
