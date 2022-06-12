95861bb1c14a8b7e74a5d632537b663ad1141868
Victor Jatobá <jatoba@credifit.com.br>
2022-06-12 14:48:40 +0000

    add chapter 3 and 4 anotations

---
+++ src/ArrayTest.java
@@ -0,0 +1,11 @@
+class ArrayTest {
+
+  public static void main(String[] args) {
+    int[] a = new int[10];
+    System.out.println(a[9]);
+    Person[] persons = new Person[10];
+    System.out.println(persons[9]);
+    int number;
+    System.out.println(number);
+  }
+}
---
+++ src/EncapsulationTest.java
@@ -0,0 +1,7 @@
+public class EncapsulationTest {
+    public static void main(String[] args) {
+        Encapsulation e = new Encapsulation();
+        int eNum = e.getNumber();
+        System.out.println(eNum);
+    }
+}
