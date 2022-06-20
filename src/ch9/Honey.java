6dbe3d44b373d2e01030550d677095f6dfe5c6bd
Victor Jatobá <jatoba@credifit.com.br>
2022-06-20 14:43:08 +0000

    chapter 9 object life cycle

---
+++ src/ch9/Honey.java
@@ -0,0 +1,22 @@
+public class Honey {
+
+  public static void main(String[] args) {
+    Honey honeyPot = new Honey();
+    Honey[] ha = { honeyPot, honeyPot, honeyPot, honeyPot };
+    System.out.println(ha[0]);
+    System.out.println(ha[1]);
+    Bees bees = new Bees();
+    bees.beeHoney = ha;
+    Bear[] bears = new Bear[5];
+    for (int i = 0; i < 5; i++) {
+      bears[i] = new Bear();
+      bears[i].hunny = honeyPot;
+    }
+    Kit kit = new Kit();
+    kit.honey = honeyPot;
+    Raccoon raccoon = new Raccoon();
+    raccoon.rh = honeyPot;
+    raccoon.rk = kit;
+    kit = null;
+  }
+}
