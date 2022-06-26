4c7be4669ab29ab007f78e50b8363655d62bdce6
Victor Jatobá <jatoba@credifit.com.br>
2022-06-26 01:30:15 +0000

    ch10: String format

---
+++ src/ch10/TestFormat.java
@@ -0,0 +1,8 @@
+public class TestFormat {
+
+  public static void main(String[] args) {
+    long myBillion = 1_000_000_000;
+    String s = String.format("%,d", myBillion);
+    System.out.println(s);
+  }
+}
