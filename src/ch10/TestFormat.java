a9d4988dce12753b7205bb761eb0206c6b1d71d9
Victor Jatobá <jatoba@credifit.com.br>
2022-06-26 01:40:03 +0000

    ch10: String format

--- src/ch10/TestFormat.java
+++ src/ch10/TestFormat.java
@@ -4,5 +4,13 @@ public class TestFormat {
     long myBillion = 1_000_000_000;
     String s = String.format("%,d", myBillion);
     System.out.println(s);
+    int myBillionInt = 129;
+
+    String s2 = String.format("%,d", myBillionInt);
+    System.out.println(s2);
+
+    boolean myBillionBool = true;
+    String s3 = String.format("%,d", myBillionBool); // it doesn't work
+    System.out.println(s3);
   }
 }
