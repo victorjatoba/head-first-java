d2ad0a6324bf641e62d96da3da106389ba672735
Victor Jatobá <jatoba@credifit.com.br>
2022-06-26 01:49:27 +0000

    ch10: String format

--- src/ch10/TestFormat.java
+++ src/ch10/TestFormat.java
@@ -4,10 +4,14 @@ public class TestFormat {
     long myBillion = 1_000_000_000;
     String s = String.format("%,d", myBillion);
     System.out.println(s);
-    int myBillionInt = 129939474;
 
+    int myBillionInt = 129939474;
     String s2 = String.format("%,d", myBillionInt);
     System.out.println(s2);
+    
+    double myDouble = 129939474.9399;
+    String sDouble = String.format("%,.2f", myDouble);
+    System.out.println(sDouble);
 
     boolean myBillionBool = true;
     String s3 = String.format("%,d", myBillionBool); // it doesn't work
