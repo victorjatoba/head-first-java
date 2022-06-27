0628449625741f6b7b68a4a77a2a4f955b7c999c
Victor Jatobá <jatoba@credifit.com.br>
2022-06-27 09:52:35 +0000

    add hashset and implement equals and hashcode methods

--- src/ch11/MockSongs.java
+++ src/ch11/MockSongs.java
@@ -19,6 +19,10 @@ public class MockSongs {
 
     System.out.println("\nSort by artist [List.sort]");
     System.out.println(songs);
+
+    System.out.println("\nUse Set to remove duplicates");
+    Set<Song> set = new HashSet<>(songs);
+    System.out.println(set);
   }
 
   public static List<Song> getSongs() {
@@ -27,11 +31,16 @@ public class MockSongs {
 
   public static void setSongs() {
     songs.add(new Song("Canjica Nova", "Jatobá"));
+    songs.add(new Song("Canjica Nova", "Jatobá"));
     songs.add(new Song("cassidy", "Artist 1"));
     songs.add(new Song("Cassidy", "Artist 2"));
     songs.add(new Song("$10", "Artist 1"));
     songs.add(new Song("10", "Artist 3"));
     songs.add(new Song("Ways", "Artist 4"));
+    songs.add(new Song("Duplicated only title", "Artist 1"));
+    songs.add(new Song("Duplicated only title", "Artist 4"));
+    songs.add(new Song("Duplicated", "Artist 1"));
+    songs.add(new Song("Duplicated", "Artist 1"));
   }
 }
 
--- src/ch11/Song.java
+++ src/ch11/Song.java
@@ -17,11 +17,23 @@ public class Song implements Comparable<Song> {
   }
 
   public String toString() {
-    return title;
+    return "'" + title + " by " + artist + "'";
   }
 
   @Override
   public int compareTo(Song o) {
     return title.compareTo(o.getTitle());
+  }
+
+  public boolean equals(Object o) {
+    if (o instanceof Song) {
+      Song s = (Song) o;
+      return title.equals(s.getTitle()) && artist.equals(s.getArtist());
+    }
+    return false;
+  }
+
+  public int hashCode() {
+    return title.hashCode() + artist.hashCode();
   }
 }
