2174e0d71b5740466f4898d36c4ea22f9eec048e
victorjatoba <victorjatoba10@gmail.com>
2022-06-27 20:22:40 -0300

    add Map of introduced by Java 9

--- src/ch11/MapTest.java
+++ src/ch11/MapTest.java
@@ -1,7 +1,31 @@
+import java.util.HashMap;
 import java.util.Map;
 
 public class MapTest {
-    static public void main(String[] args) {
-        Map<String, Integer> rank = Map.of("Sena", 10, "Barrichello", 2);
-    }
+
+  public static void main(String[] args) {
+    Map<String, Integer> rank = getMapNormal();
+
+    Map<String, Integer> rankConvenienceFactoryJava9 = getMapUsingConvenienceFactoryByJava9();
+
+    System.out.println(rank);
+    System.out.println(rankConvenienceFactoryJava9);
+  }
+
+  private static Map<String, Integer> getMapUsingConvenienceFactoryByJava9() {
+    Map<String, Integer> rankConvenienceFactoryJava9 = Map.of(
+      "Sena",
+      10,
+      "Barrichello",
+      2
+    );
+    return rankConvenienceFactoryJava9;
+  }
+
+  private static Map<String, Integer> getMapNormal() {
+    Map<String, Integer> rank = new HashMap<>();
+    rank.put("Sena", 10);
+    rank.put("Barrichello", 2);
+    return rank;
+  }
 }
