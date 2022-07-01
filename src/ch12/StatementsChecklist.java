a87ba4899a8ede9b3bc3863181d7b1b2769b354e
victorjatoba <victorjatoba10@gmail.com>
2022-06-30 21:19:57 -0300

    add personal code for functional interface usage

--- src/ch12/StatementsChecklist.java
+++ src/ch12/StatementsChecklist.java
@@ -1,3 +1,4 @@
+import java.security.Principal;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
@@ -6,9 +7,44 @@ public class StatementsChecklist {
 
   public static void main(String[] args) {
     Runnable r = () -> System.out.println("Hi!");
-    Consumer c = s -> System.out.println(s);
+    Consumer<Rabit> c = s -> System.out.println(s.size);
     // Supplier<String> s = () -> System.out.println("Some String"); // Should return a String but doesn't
     // Consumer<String> c = (s1, s2) -> System.out.println(s1 + s2); // Should take only one parameter but has two
+    // Runnable r2 = (String str) -> System.out.println(str); // Should not have parameter
     Function<String, Integer> f = s -> s.length();
+    Supplier<String> s = () -> "Some string";
+    r.run();
+    c.accept(new Rabit(2));
+    System.out.println(s.get());
+
+    CompleteName<User> completenameName = u ->
+      System.out.println(u.firstName + " " + u.lastName);
+
+    completenameName.print(new User("Victor", "Jatobá"));
   }
+}
+
+class Rabit {
+
+  int size;
+
+  Rabit(int size) {
+    this.size = size;
+  }
+}
+
+class User {
+
+  String firstName;
+  String lastName;
+
+  User(String firstName, String lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+}
+
+@FunctionalInterface
+abstract interface CompleteName<T> {
+  abstract void print(T data);
 }
